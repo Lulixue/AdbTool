29613a6bf5c285c2059f92b2f53c2482a0f4071b
lulixue <xiaoligen@fts-sz-oa.com>
2018-06-19 15:18:34 +0800

    add more phone informations

--- AdbTool/DialogPhoneInfo.cpp
+++ AdbTool/DialogPhoneInfo.cpp
@@ -26,22 +26,43 @@ void CDialogPhoneInfo::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_EDIT_PHONE_INFO, m_editPhoneInfo);
+	DDX_Control(pDX, IDC_COMBO_INFO_TYPES, m_cbInfoTypes);
 }
 
 
 BEGIN_MESSAGE_MAP(CDialogPhoneInfo, CDialogEx)
 	ON_WM_SHOWWINDOW()
 	ON_WM_NCDESTROY()
+	ON_CBN_SELCHANGE(IDC_COMBO_INFO_TYPES, &CDialogPhoneInfo::OnCbnSelchangeComboInfoTypes)
+	ON_WM_NCCREATE()
 END_MESSAGE_MAP()
 
 
 // CDialogPhoneInfo 消息处理程序
-void CDialogPhoneInfo::GetPhoneInfo()
+void CDialogPhoneInfo::GetPhoneInfo(int type)
 {
 	PARAM_T para;
-	para.strCmd = TEXT("adb shell getprop");
+	
 	para.nType = CMD_INFINITE | CMD_READ_BACK_FILE;
 
+	switch (type) {
+	case AINFO_CPUS:
+		para.strCmd = TEXT("adb shell cat /proc/cpuinfo");
+		break;
+	case AINFO_MEM:
+		para.strCmd = TEXT("adb shell cat /proc/meminfo");
+		break;
+	case AINFO_VERSION:
+		para.strCmd = TEXT("adb shell cat /proc/version");
+		break;
+	case AINFO_CMDLINE:
+		para.strCmd = TEXT("adb shell cat /proc/cmdline");
+		break;
+	case AINFO_PROP:
+	default:
+		para.strCmd = TEXT("adb shell getprop");
+		break;
+	}
 	CAdbInterface::CreateAdbProcess(&para);
 
 	m_editPhoneInfo.SetWindowTextW(para.strReturn);
@@ -51,7 +72,7 @@ void CDialogPhoneInfo::OnShowWindow(BOOL bShow, UINT nStatus)
 {
 	CDialogEx::OnShowWindow(bShow, nStatus);
 
-	GetPhoneInfo();
+	GetPhoneInfo(m_cbInfoTypes.GetCurSel());
 }
 
 
@@ -60,4 +81,32 @@ void CDialogPhoneInfo::OnNcDestroy()
 	CDialogEx::OnNcDestroy();
 
 	delete this;
+}
+
+
+void CDialogPhoneInfo::OnCbnSelchangeComboInfoTypes()
+{
+	GetPhoneInfo(m_cbInfoTypes.GetCurSel());
+}
+
+BOOL CDialogPhoneInfo::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	m_cbInfoTypes.AddString(ANDROID_INFO_PROP);
+	m_cbInfoTypes.AddString(ANDROID_INFO_CPUS);
+	m_cbInfoTypes.AddString(ANDROID_INFO_MEM);
+	m_cbInfoTypes.AddString(ANDROID_INFO_VERSION);
+	m_cbInfoTypes.AddString(ANDROID_INFO_CMDLINE);
+	m_cbInfoTypes.SetCurSel(0);
+
+	return TRUE;
+}
+
+BOOL CDialogPhoneInfo::OnNcCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (!CDialogEx::OnNcCreate(lpCreateStruct))
+		return FALSE;
+
+	return TRUE;
 }
--- AdbTool/DialogPhoneInfo.h
+++ AdbTool/DialogPhoneInfo.h
@@ -1,7 +1,19 @@
 #pragma once
 #include "afxwin.h"
 
+#define ANDROID_INFO_PROP		TEXT("Properties")
+#define ANDROID_INFO_CPUS		TEXT("Cpus")
+#define ANDROID_INFO_MEM		TEXT("Memories")
+#define ANDROID_INFO_VERSION	TEXT("Version")
+#define ANDROID_INFO_CMDLINE	TEXT("Cmdline")
 
+enum {
+	AINFO_PROP = 0,
+	AINFO_CPUS,
+	AINFO_MEM,
+	AINFO_VERSION,
+	AINFO_CMDLINE,
+};
 // CDialogPhoneInfo 对话框
 
 class CDialogPhoneInfo : public CDialogEx
@@ -17,11 +29,15 @@ public:
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
-	void GetPhoneInfo();
+	void GetPhoneInfo(int type);
+	BOOL OnInitDialog();
 
 	DECLARE_MESSAGE_MAP()
 public:
 	CEdit m_editPhoneInfo;
 	afx_msg void OnShowWindow(BOOL bShow, UINT nStatus);
 	afx_msg void OnNcDestroy();
+	CComboBox m_cbInfoTypes;
+	afx_msg void OnCbnSelchangeComboInfoTypes();
+	afx_msg BOOL OnNcCreate(LPCREATESTRUCT lpCreateStruct);
 };
